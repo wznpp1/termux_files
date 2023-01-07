cb9fc93c5a9de168f0dd5eb05c52b5462993672b
wznpp1 <wznpp1@hotmail.com>
2023-01-07 13:47:21 +0800

    更新 termux.sh

--- termux.sh
+++ termux.sh
@@ -26,6 +26,13 @@ if [[ ! -x /data/data/com.termux/files/home/app/bin/udocker ]]; then
         chmod +x ~/app/bin/*
         curl -L https://github.com/wznpp1/termux_files/raw/main/app/udocker/udocker.sh > /data/data/com.termux/files/home/app/udocker/udocker.sh
     fi
+
+    if [[ ! -x /data/data/com.termux/files/home/app/udocker/udocker.sh ]]; then
+        mkdir -p ~/app/udocker/udocker
+        curl -L https://github.com/wznpp1/termux_files/raw/main/app/udocker/udocker.sh > /data/data/com.termux/files/home/app/udocker/udocker.sh
+        chmod +x ~/app/udocker/udocker.sh
+    fi
+    
     #/bin/bash ~/app/udocker/udocker.sh
     chmod +x ~/app/bin/udocker
 fi
