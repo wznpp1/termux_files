b1e5eef0e78af9d4eb868813047dfe2511407f7a
wznpp1 <wznpp1@hotmail.com>
2024-04-03 12:05:05 +0800

    1

---
+++ shenzhen copy/gost.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#curl -fsSL https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/shenzhen/gost.sh | bash -x
+#/etc/netplan
+#netplan apply
+
+#ifconfig eth0:1 192.168.0.2 up
+#source <(curl -L https://go-install.netlify.app/install.sh)
+
+
+mkdir -p /root/app/gost
+cd /root/app/gost/ 
+
+wget https://gh-proxy.com/https://github.com/go-gost/gost/releases/download/v3.0.0-rc6/gost_3.0.0-rc6_linux_amd64.tar.gz
+tar -zxvf gost_3.0.0-rc6_linux_amd64.tar.gz
+wget -O /root/app/gost/001.yaml  https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/shenzhen/001.yaml
+
+chmod +x /root/app/gost/gost
+apt-get update
+apt-get install -y supervisor
+
+systemctl stop supervisor.service
+cp /etc/supervisor/supervisord.conf /etc/supervisor/supervisord.conf.bak
+cd /etc/supervisor
+wget -O /etc/supervisor/supervisord.conf  https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/supervisord1.conf
+systemctl enable supervisor.service
+
+systemctl stop supervisor.service
+systemctl start supervisor.service
+supervisorctl update
+supervisorctl start gost
