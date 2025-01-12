84cb0bf665065b3fb5b45d4f761f2878995b33ce
wznpp1 <wznpp1@hotmail.com>
2025-01-12 23:07:11 +0800

    1

--- huaweiyun/guangzhou/gost.sh
+++ huaweiyun/guangzhou/gost.sh
@@ -16,6 +16,8 @@ wget -O /root/app/gost/001.yaml  https://gh-proxy.com/https://github.com/wznpp1/
 wget -O /root/app/gost/VLANSet.sh  https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/huaweiyun/guangzhou/VLANSet.sh
 wget -O /etc/systemd/system/VLANSet.service  https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/huaweiyun/guangzhou/VLANSet.service
 
+wget -O /etc/systemd/system/Gost.service  https://gh-proxy.com/https://github.com/wznpp1/termux_files/raw/main/huaweiyun/guangzhou/Gost.service
+
 chmod +x /root/app/gost/VLANSet.sh
 chmod +x /root/app/gost/gost
 
